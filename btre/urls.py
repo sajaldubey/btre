734da1e561397007423228393f15b991b785d079
Sajal Dubey <dubeysajal@gmail.com>
2022-08-09 00:12:59 +0530

    added new models

--- btre/urls.py
+++ btre/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('pages.urls')),
     path('listings/', include('listings.urls')),
-    path('realtors/', include('realtors.urls')),
+    # path('realtors/', include('realtors.urls')),
     path('admin/', admin.site.urls),
 ]
